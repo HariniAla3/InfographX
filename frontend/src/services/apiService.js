b498b6f52b949be77f58cd635ebb8cff320138ab
[HariniAla3] <[hariniala@utexas.edu]>
2024-12-11 23:31:33 -0600

    Updated apiService

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://infographx-backend.onrender.com/";
+const API_BASE_URL = "https://infographx-backend.onrender.com";
 
 // export const uploadData = async (file) => {
 //   const formData = new FormData();
